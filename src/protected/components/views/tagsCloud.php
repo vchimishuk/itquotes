9c182d5342b5d832b8415f267243bf2e012685ea
Viacheslav Chumushuk <voice@root.ua>
2009-12-07 00:21:04 +0200

    Add rewrite rule for more easy admin access.

--- src/protected/components/views/tagsCloud.php
+++ src/protected/components/views/tagsCloud.php
@@ -1,3 +1,3 @@
 <? foreach($tags as $tag): ?>
-<?=CHtml::link("{$tag->name} ({$tag->approvedQuotesCount})", array("site/tag", 'tag' => CHtml::encode($tag->name)))?>,<br />
+  <?=CHtml::link("{$tag->name} ({$tag->approvedQuotesCount})", array("site/tag", 'tag' => CHtml::encode($tag->name)))?><br />
 <? endforeach; ?>
