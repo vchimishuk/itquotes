7184cc3391dd3db29a453209a9460455cde146f1
Viacheslav Chumushuk <voice@root.ua>
2010-04-09 20:04:25 +0300

    Added table for rss statistics. Now every rss request saved into this table.

--- src/protected/controllers/site/RssAction.php
+++ src/protected/controllers/site/RssAction.php
@@ -13,6 +13,14 @@ class RssAction extends CAction
 		
 		$quotes = Quote::model()->with('author')->findAll($criteria);
 
+		// Save statistics.
+		$req = new RssRequest();
+		$req->ip = '127.0.0.1';
+		$req->requestTime = time();
+		$req->userAgent = 'ua';
+		$req->os = 'os';
+		$req->save();
+
 		$this->controller->render('rss', array(
 			'quotes' => $quotes,
 			'config' => $config,
---
+++ src/protected/models/RssRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+class RssRequest extends CActiveRecord
+{
+	/**
+	 * The followings are the available columns in table 'RssRequest':
+	 * @var integer $id
+	 * @var string $requestTime
+	 * @var string $userAgent
+	 * @var string $os
+	 * @var string $ip
+	 */
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return CActiveRecord the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'RssRequest';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return array(
+			array('userAgent','length','max'=>255),
+			array('os','length','max'=>255),
+			array('ip','length','max'=>15),
+			array('requestTime', 'required'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'Id',
+			'requestTime' => 'Request Time',
+			'userAgent' => 'User Agent',
+			'os' => 'Os',
+			'ip' => 'Ip',
+		);
+	}
+}
